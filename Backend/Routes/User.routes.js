e032b98d06b4db4b5a769b6d239005226168215f
Imran Khan <99397606+imran120198@users.noreply.github.com>
2023-04-09 16:42:45 +0530

    Delete a user by id

--- Backend/Routes/User.routes.js
+++ Backend/Routes/User.routes.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 const UserRoute = Router();
 const { UserModel } = require("../Model/User.model");
 
-
 //Create a new user.
 UserRoute.post("/users", async (req, res) => {
   try {
@@ -13,7 +12,7 @@ UserRoute.post("/users", async (req, res) => {
     await user.save();
     res.status(201).send(user);
   } catch (err) {
-    res.status(400).send({ message: err.message });
+    res.status(404).send({ message: err.message });
     console.log({ message: err.message });
   }
 });
@@ -25,22 +24,40 @@ UserRoute.get("/users/:id", async (req, res) => {
     if (userData) {
       res.status(201).send(userData);
     } else {
-      res.status(400).send("User Not Found");
+      res.status(404).send("User Not Found");
     }
   } catch (err) {
     console.log({ message: err.message });
   }
 });
 
-// //Update a user's name or bio by id.
-// UserRoute.put("/users/:id", async(req,res) => {
-//     try{
+//Update a user's name or bio by id.
+// UserRoute.put("/users/:id", async (req, res) => {
+//   try {
+//     const userEdit = await UserModel.findById(req.params.id);
+//     if (!userEdit) {
+//       res.status(404).send({ message: "User Data not found" });
+//     } else {
 
 //     }
-//     catch(err){
-//         console.log({message: err.message})
-//     }
-// })
+//   } catch (err) {
+//     console.log({ message: err.message });
+//   }
+// });
+
+//Delete a user by id.
+UserRoute.delete("/users/:id", async (req, res) => {
+  try {
+    const deleteUser = await UserModel.findByIdAndDelete(req.params.id);
+    if (!deleteUser) {
+      res.status(400).send({ message: "Something went wrong!" });
+    } else {
+      res.status(201).send({ message: "Successfully Delete User info" });
+    }
+  } catch (err) {
+    res.status(404).send({ message: err.message });
+  }
+});
 
 module.exports = {
   UserRoute,
