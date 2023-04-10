59208967df1574bfcd066b70afa1b52f1c3b2eae
Imran Khan <99397606+imran120198@users.noreply.github.com>
2023-04-10 07:16:39 +0530

    functionality: Added Edit functionality in user list

--- Frontend/frontend/src/Components/UserList.jsx
+++ Frontend/frontend/src/Components/UserList.jsx
@@ -3,25 +3,66 @@ import {
   Box,
   Button,
   Center,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Table,
   TableContainer,
   Tbody,
   Td,
   Text,
+  Textarea,
   Th,
   Thead,
   Tr,
+  useDisclosure,
 } from "@chakra-ui/react";
 import axios from "axios";
 
 const UserList = () => {
   const [data, setData] = useState([]);
+  const [modify, setModify] = useState({});
+  const [edit, setedit] = useState({
+    name: "",
+    bio: "",
+  });
+
+  const {
+    isOpen: iseditOpen,
+    onOpen: oneditOpen,
+    onClose: oneditClose,
+  } = useDisclosure();
 
   useEffect(() => {
     axios("https://adobe-backend-ek2e.onrender.com/users").then((res) => {
       setData(res.data);
     });
   }, [data]);
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setedit({ ...edit, [name]: value });
+  };
+
+  const handleEdit = (e) => {
+    e.preventDefault()
+    axios
+      .put(`https://adobe-backend-ek2e.onrender.com/users/${modify._id}`, {
+        name: edit.name,
+        bio: edit.bio,
+      })
+      .then((res) => {
+        console.log("Edited");
+      })
+      .catch((err) => console.log(err));
+  };
 
   const handleDelete = (e) => {
     axios
@@ -65,7 +106,15 @@ const UserList = () => {
                       <Td>{elem.email}</Td>
                       <Td>{elem.bio}</Td>
                       <Td>
-                        <Button colorScheme="green">Edit</Button>
+                        <Button
+                          onClick={() => {
+                            setModify(elem)
+                            oneditOpen();
+                          }}
+                          colorScheme="green"
+                        >
+                          Edit
+                        </Button>
                       </Td>
                       <Td>
                         <Button
@@ -81,6 +130,47 @@ const UserList = () => {
             </Tbody>
           </Table>
         </TableContainer>
+
+        {modify && (
+          <Modal onClose={oneditClose} isOpen={iseditOpen}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Edit Name and Bio</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <FormControl isRequired>
+                  <FormLabel>Name</FormLabel>
+                  <Input
+                    type="text"
+                    name="name"
+                    onChange={handleChange}
+                    maxLength={50}
+                    placeholder="Enter Name"
+                  />
+                  <FormLabel>Bio</FormLabel>
+                  <Textarea
+                    name="bio"
+                    type="text"
+                    onChange={handleChange}
+                    maxLength={200}
+                    placeholder="Enter bio"
+                  />
+                </FormControl>
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  onClick={(e) => {
+                    handleEdit(e);
+                    oneditClose();
+                  }}
+                >
+                  Submit
+                </Button>
+                <Button onClick={oneditClose}>Close</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        )}
       </Box>
     </div>
   );
